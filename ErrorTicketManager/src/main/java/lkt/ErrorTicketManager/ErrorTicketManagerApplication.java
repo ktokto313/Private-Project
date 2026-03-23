bffc1c0ed8d00652eed455134f81eae6b31285fb
ktokto313 <kto3132006@gmail.com>
2026-03-23 10:20:54 +0700

    Application Init by spring initializr

---
+++ ErrorTicketManager/src/main/java/lkt/ErrorTicketManager/ErrorTicketManagerApplication.java
@@ -0,0 +1,13 @@
+package lkt.ErrorTicketManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrorTicketManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErrorTicketManagerApplication.class, args);
+	}
+
+}
---
+++ ErrorTicketManager/src/test/java/lkt/ErrorTicketManager/ErrorTicketManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package lkt.ErrorTicketManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErrorTicketManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
