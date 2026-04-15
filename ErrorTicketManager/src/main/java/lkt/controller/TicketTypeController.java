2429fba59b99a9bf270ce4ecf09e576f30db64c2
ktokto313 <kto3132006@gmail.com>
2026-04-15 10:48:49 +0700

    change ticketType api name to ticket-type

--- ErrorTicketManager/src/main/java/lkt/controller/TicketTypeController.java
+++ ErrorTicketManager/src/main/java/lkt/controller/TicketTypeController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/ticketType")
+@RequestMapping("/api/ticket-type")
 public class TicketTypeController {
     @Autowired
     private ITicketTypeService ticketTypeService;
